695278dd2b8ad0b42a09c7a6b16195f14a013560
Yinks87 <105646695+Yinks87@users.noreply.github.com>
2024-04-04 13:25:42 +0200

    Update config.js

--- backend/config_file/config.js
+++ backend/config_file/config.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 /**
  * @typedef EnvironmentConfiguration
- * @prop {string} SERVER_HOST
- * @prop {string} SERVER_PORT
+ * @prop {string} SERVER_HOST Express server host
+ * @prop {string} SERVER_PORT Express server port
  * @prop {string} 
  * @prop {string} 
  * @prop {string} 
